e27d2b49ef352a4750c6c8b16043a39008aa4d74
smithe84 <smithe84@wwu.edu>
2025-04-21 09:52:20 -0700

    first attempt at commiting

---
+++ config/boards/arm/mod_/board.cmake
@@ -0,0 +1,10 @@
+#
+# copyright (c) 2025 ZMK project contributors
+# SPDX-license-identifier: MIT
+#
+
+# cmake directives on how to flash microcontroller
+board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
+include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/uf2.board.cmake)
