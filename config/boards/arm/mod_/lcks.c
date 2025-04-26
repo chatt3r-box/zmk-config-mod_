884e92ec0c446221b2e8d100463d0ddd6f9732e4
smithe84 <smithe84@wwu.edu>
2025-04-26 13:44:01 -0700

    Added lock LED functionality.

---
+++ config/boards/arm/mod_/lcks.c
@@ -0,0 +1,47 @@
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/drivers/led.h>
+#include <zephyr/kernel.h>
+#include <zephyr/init.h>
+#include <zmk/hid_indicators.h>
+#include <zmk/events/hid_indicators_changed.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
+
+#define LED_GPIO_NODE_ID DT_COMPAT_GET_ANY_STATUS_OKAY(gpio_leds)
+
+// GPIO-based LED device
+static const struct device *led_dev = DEVICE_DT_GET(LED_GPIO_NODE_ID);
+
+static int led_scroll_lock_listener_cb(const zmk_event_t *eh) {
+    zmk_hid_indicators_t flags = zmk_hid_indicators_get_current_profile();
+
+    // enable scroll lock LED indicator
+    if (flags & HID_USAGE_LED_SCROLL_LOCK) {
+        LOG_INF("Scroll lock is on");
+        led_on(led_dev, DT_NODE_CHILD_IDX(DT_ALIAS(scroll_lock_led)));
+
+    // disable scroll lock LED indicator
+    } else {
+        LOG_INF("Scroll lock is off");
+        led_off(led_dev, DT_NODE_CHILD_IDX(DT_ALIAS(scroll_lock_led)));
+    }
+
+    return 0;
+}
+
+ZMK_LISTENER(led_indicators_listener, led_scroll_lock_listener_cb);
+ZMK_SUBSCRIPTION(led_indicators_listener, zmk_hid_indicators_changed);
+
+static int leds_init(const struct device *device) {
+    if (!device_is_ready(led_dev)) {
+        LOG_ERR("Device %s is not ready", led_dev->name);
+        return -ENODEV;
+    }
+
+    return 0;
+}
+
+// run leds_init on boot
+SYS_INIT(leds_init, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
