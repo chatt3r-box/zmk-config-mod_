ae5974afbab88684ab284df4918d3c946b752be2
smithe84 <smithe84@wwu.edu>
2025-04-27 11:51:03 -0700

    Test remove of logging for NP.

--- config/boards/arm/mod_/lckn.c
+++ config/boards/arm/mod_/lckn.c
@@ -5,9 +5,6 @@
 #include <zephyr/init.h>
 #include <zmk/hid_indicators.h>
 #include <zmk/events/hid_indicators_changed.h>
-#include <zephyr/logging/log.h>
-
-LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 #define LED_GPIO_NODE_ID DT_COMPAT_GET_ANY_STATUS_OKAY(gpio_leds)
 
@@ -19,12 +16,10 @@ static int led_num_lock_listener_cb(const zmk_event_t *eh) {
 
     // enable num lock LED indicator
     if (flags & HID_USAGE_LED_NUM_LOCK) {
-        LOG_INF("Num lock is on");
         led_on(led_dev, DT_NODE_CHILD_IDX(DT_ALIAS(num_lock_led)));
 
     // disable num lock LED indicator
     } else {
-        LOG_INF("Num lock is off");
         led_off(led_dev, DT_NODE_CHILD_IDX(DT_ALIAS(num_lock_led)));
     }
 
@@ -36,7 +31,6 @@ ZMK_SUBSCRIPTION(led_indicators_listener, zmk_hid_indicators_changed);
 
 static int leds_init(const struct device *device) {
     if (!device_is_ready(led_dev)) {
-        LOG_ERR("Device %s is not ready", led_dev->name);
         return -ENODEV;
     }
 
